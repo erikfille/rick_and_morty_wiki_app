988d0f88fa679d2851ba7f00ae85afb6a63bc870
erikfille <erikfilleaudeau@gmail.com>
2022-11-16 17:30:50 -0300

    Styled Components + Variables

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,6 @@ const DivStyle = styled.div`
   scale: 125%;
 
   button {
-    font-weight: bold;
     padding: 3px 8px;
     color: ${superLightBlue};
     border: none;
@@ -16,26 +15,23 @@ const DivStyle = styled.div`
     border-radius: 0px 5px 5px 0px;
     background: linear-gradient(145deg, ${lightBlue}, ${darkBlue});
     box-shadow: 0px 0px 2px ${darkBlue}, 0px 0px 2px ${lightBlue};
-    z-index: 1;
-    transition: 0.75s;
-
-
 
     &:hover{
-    color: ${darkBlue};
-    border-radius: 0px 5px 5px 0px;
-    background: linear-gradient(145deg, #f0f0f0, #cacaca);
-    box-shadow: 0px 0px 2px #5a5a5a, 0px 0px 2px #ffffff;
-    transition: 0.75s;
+    font-weight: bold;
+
+    // color: ${darkBlue};
+    // border-radius: 0px 5px 5px 0px;
+    // background: linear-gradient(145deg, #f0f0f0, #cacaca);
+    // box-shadow: 0px 0px 2px #5a5a5a, 0px 0px 2px #ffffff;
+    // transition: 0.75s;
     }
   }
 
   input {
    border-radius: 5px 0px 0px 5px;
    background: linear-gradient(145deg, #f0f0f0, #cacaca);
-   box-shadow: 0px 0px 2px #5a5a5a, 0px 0px 2px #ffffff;
+  //  box-shadow: 0px 0px 2px #5a5a5a, 0px 0px 2px #ffffff;
    scale: 103%;
-   z-index: 2;
   }
 `;
 
@@ -48,4 +44,4 @@ export default function SearchBar(props) {
       </button>
     </DivStyle>
   );
-}
+};
